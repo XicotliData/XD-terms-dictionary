9787d3ea5cb0d983562098e6067b37db0c104be2
Juan M Barrios <j.m.barrios@gmail.com>
2022-11-14 15:19:36 -0600

    feat: observation field values included
    observation field allowed values were included in the rendered
    files

--- src/xd_dictionary/xd_dictionary.py
+++ src/xd_dictionary/xd_dictionary.py
@@ -3,8 +3,10 @@
 this module exposes the build process for the XicotliData
 dictionary static site.
 """
+import importlib.resources
 import logging
 from pathlib import Path
+from shutil import copytree
 import sys
 from typing import BinaryIO, Union
 import click
@@ -41,7 +43,7 @@ def process_xd_dict(xd_dict_path: BinaryIO) -> pd.DataFrame:
                            "Observación / Descripción del campo": "description"
                         })
                        .loc[:,
-                            ["observationFieldID", "of_name", "of_id", "url"]]
+                            ["observationFieldID", "of_name", "of_id", "url", "description"]]
                        .assign(of_values = lambda x: x["observationFieldID"].apply(get_values_of,
                                                                                    args=(values_obs_fields_data,)))
                       )
@@ -81,11 +83,14 @@ def main(
     if not build_dir:
         click.echo(terms_tmpl.render(terms=xd_data.to_dict(orient="records")))
         return
+    static_path = importlib.resources.path("xd_dictionary.templates", "static")
+    logging.debug(f"static path: {static_path}")
 
     build_dir.mkdir(exist_ok=True)
+    copytree(str(static_path), build_dir / "static", dirs_exist_ok=True)
     fn = build_dir / "index.html"
     with fn.open('w') as f:
-        f.write(terms_tmpl.render(terms=xd_data.to_dict(orient="records")))
+        f.write(terms_tmpl.render(terms=xd_data.to_dict(orient="records"), output_dir=True))
 
 if __name__ == "__main__":
     pass
